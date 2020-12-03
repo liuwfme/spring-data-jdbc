176581486928c252edbc3ee938fff5e1bb842498
Greg L. Turnquist <gturnquist@vmware.com>
2020-12-03 12:37:13 -0600

    try

--- test.sh
+++ test.sh
@@ -7,5 +7,5 @@ mkdir -p /tmp/jenkins-home
 chown -R 1001:1001 .
 MAVEN_OPTS="-Duser.name=jenkins -Duser.home=/tmp/jenkins-home" \
   ./mvnw \
-  -Dregistry.username=${DOCKER_HUB_USR} -Dregistry.password=${DOCKER_HUB_PSW} \
+#  -Dregistry.username=${DOCKER_HUB_USR} -Dregistry.password=${DOCKER_HUB_PSW} \
   -Pci,all-dbs clean dependency:list test -Dsort -U -B
